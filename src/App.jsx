609936f7b4e03642cf900d42fbf11b2f29c3b5d3
Ma-Eum <cl.gachelin@gmail.com>
2025-01-21 14:55:23 +0100

    lien pour afficher les données de json

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
-import Error from './pages/Error/Error'; // Import de la page Error
+import Error from './pages/Error/Error';
+import Accommodation from './pages/Accommodation/Accommodation'; // Page des logements
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Route pour la page d'accueil */}
         <Route path="/" element={<Home />} />
-
-        {/* Route pour la page À Propos */}
         <Route path="/about" element={<About />} />
-
-        {/* Route pour les pages non trouvées */}
+        <Route path="/logement/:id" element={<Accommodation />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,46 +2,8 @@ import Header from '../../components/Header/Header';
 import Banner from '../../components/Banner/Banner';
 import Card from '../../components/Card/Card';
 import Footer from '../../components/Footer/Footer';
+import accommodations from '../../data/accommodations.json'; // Import des données JSON
 import './Home.scss'; // Import des styles spécifiques à la page Home
-
-const housingData = [
-  {
-    id: 1,
-    image: "",
-    title: 'Titre de la location 1',
-    link: '/logement1',
-  },
-  {
-    id: 2,
-    image: "",
-    title: 'Titre de la location 2',
-    link: '/logement2',
-  },
-  {
-    id: 3,
-    image: "",
-    title: 'Titre de la location 3',
-    link: '/logement3',
-  },
-  {
-    id: 4,
-    image: "",
-    title: 'Titre de la location 4',
-    link: '/logement4',
-  },
-  {
-    id: 5,
-    image: "",
-    title: 'Titre de la location 5',
-    link: '/logement5',
-  },
-  {
-    id: 6,
-    image: "",
-    title: 'Titre de la location 5',
-    link: '/logement5',
-  },
-];
 
 const Home = () => {
   return (
@@ -56,15 +18,15 @@ const Home = () => {
         {/* Liste des logements */}
         <div className="housing housing-index">
           <section className="housing-grid">
-            {housingData.map(({ id, image, title, link }) => (
-              <Card key={id} image={image} title={title} link={link} />
+            {accommodations.map(({ id, cover, title }) => (
+              <Card key={id} image={cover} title={title} link={`/logement/${id}`} />
             ))}
           </section>
         </div>
       </main>
 
-       {/* Footer */}
-       <Footer />
+      {/* Footer */}
+      <Footer />
     </div>
   );
 };
